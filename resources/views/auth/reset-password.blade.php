50dd57860a5513ab74caa6ba51a50e5ea274eefb
Luiz Felipe <felipeceltic@gmail.com>
2024-01-05 11:39:24 -0300

    CHORE: Resetar senha

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,37 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
-        <x-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
-
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+@extends('layouts.guest')
+@section('content')
+    <div class="d-flex justify-content-center py-3">
+        <x-authentication-card-logo />
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="card col-4">
+            <div class="card-body">
+                <form method="POST" action="{{ route('password.update') }}">
+                    @csrf
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail</label>
+                        <input type="email" class="form-control" id="email" name="email"
+                        :value="old('email', $request->email)" required autofocus autocomplete="username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Senha</label>
+                        <input type="password" class="form-control" name="password" id="password" required
+                            autocomplete="new-password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Confirmar senha</label>
+                        <input type="password" class="form-control" name="password_confirmation" id="password_confirmation"
+                            required autocomplete="new-password">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <div class="d-flex justify-content-between">
+                            <button type="submit" class="btn btn-primary">Concluir</button>
+                            <a class="text-decoration-none" href="{{ route('login') }}">Já possui cadastro?</a>
+                        </div>
+                    </div>
+                </form>
             </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+        </div>
+    </div>
+@endsection
